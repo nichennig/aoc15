21cd3a88bd88acebd8637a67071758a09acc35bd
nichennig <vinzent.hennig@gmail.com>
2020-12-16 09:22:50 +0100

    init aoc day 15

--- src/aoc15.jl
+++ src/aoc15.jl
@@ -1,5 +1,34 @@
 module aoc15
 
+export spoken, playUpToTurn
+
 # Write your package code here.
+function spoken(inputs, turn)
+    if turn <= length(inputs)
+        return inputs[turn]
+    end
+    # play the first rounds with the given input
+    turnMemory = Dict() # the key is a spoken number and the value is the last round this key is spoken
+    latestTurn = length(inputs)
+    latestNumber = inputs[latestTurn]
+    for i in eachindex(inputs)
+        turnMemory[inputs[i]] = i
+    end
+    # play the remaining rounds
+    return playUpToTurn(turnMemory,latestNumber,latestTurn,turn)
+end
+
+function playUpToTurn(relevantTurns, currentNumber, from, upTo)
+    roundResult = 0
+    if haskey(relevantTurns,currentNumber)
+        roundResult = from - relevantTurns[currentNumber]
+    end
+    relevantTurns[currentNumber] = from
+    if from < (upTo-1)
+        return playUpToTurn(relevantTurns, roundResult, from + 1, upTo)
+    else
+        return roundResult
+    end 
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,38 @@
 using aoc15
 using Test
 
-@testset "aoc15.jl" begin
+@testset "memoryGameTests" begin
+    
+    @testset "startingListIsSpoken" begin
     # Write your tests here.
+        input = [4,1]
+        @test aoc15.spoken(input, 1) == 4
+        @test aoc15.spoken(input, 2) == 1 
+    end
+
+    @testset "newNumberWasAdded" begin
+        input = [0,3,6]
+        @test aoc15.spoken(input, 4) == 0
+        @test aoc15.spoken(input, 8) == 0
+        @test aoc15.spoken(input, 10) == 0
+    end
+
+    @testset "oldNumberIsFound" begin
+        input = [0,3,6]
+        @test aoc15.spoken(input, 5) == 3
+        @test aoc15.spoken(input, 6) == 3
+        @test aoc15.spoken(input, 7) == 1
+        @test aoc15.spoken(input, 9) == 4
+    end
+
+    @testset "2020Examples" begin
+        @test aoc15.spoken([0,3,6], 2020) == 436
+        @test aoc15.spoken([1,3,2], 2020) == 1
+        @test aoc15.spoken([2,1,3], 2020) == 10
+        @test aoc15.spoken([1,2,3], 2020) == 27
+        @test aoc15.spoken([2,3,1], 2020) == 78
+        @test aoc15.spoken([3,2,1], 2020) == 438
+        @test aoc15.spoken([3,1,2], 2020) == 1836
+    end
+
 end
